d45be99f6b9c4b6297b51d1e63318ccf6ccb1edb
Alberto Morelli Molina <almorellim@gmail.com>
2021-09-09 19:17:22 -0300

    nueva version 2

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+      $("#botonsito").click(function(){
+          alert("Gracias por enviar tus datos!!");
+      }); 
+  });
